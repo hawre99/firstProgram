6d77c77de4b6d269e3457c5024714cb71f96f23d
hawre99 <77396013+hawre99@users.noreply.github.com>
2021-01-14 16:40:03 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProgram
+{
+   public class Program
+    {
+       public static void Main(string[] args)
+        {
+            int year = 2021;
+            Console.WriteLine("Please enter your Name :");
+            string name = Console.In.ReadLine();
+            Console.WriteLine("year of birt :");
+            String age = Console.In.ReadLine();
+            int conAge = int.Parse(age);
+
+            int yearOfBirth = year - conAge;
+
+            Console.WriteLine("your Name is " + name);
+            Console.WriteLine("your age is " + yearOfBirth);
+        }
+    }
+}
